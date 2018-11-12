30a1ab2663ed272b9127e5ed82fd67f8a6834d78
misolee <mzxmiso@gmail.com>
2018-11-12 15:28:31 -0800

    Add FirstRoutes rails project W4D1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: users 
+  end
+  
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else 
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+  
+  # def edit
+  #   render json: params
+  # end
+  def update 
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: user
+    else 
+      render json: user.error.full_messages, status: 418
+    end 
+  end
+  
+  def show
+    user = User.find(params[:id])
+    render json: user 
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end 
+  
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render json: user 
+  end
+end
